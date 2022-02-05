35fbcfa5f739a1c26f768cfd665117c2cb63a9b6
rivaldo souza <rivaldosilva@si.fiponline.edu.br>
2022-02-04 21:38:04 -0300

    body-parser cors consign

---
+++ Projeto/backend/config/routes.js
@@ -0,0 +1,5 @@
+module.exports = app =>{
+
+    app.route('/users')
+        .post(app.api.user.save)
+}
--- Projeto/backend/index.js
+++ Projeto/backend/index.js
@@ -1,4 +1,11 @@
 const app = require('express')() //ja chama o resultado do require
+const consign = require('consign')
+
+consign()
+    .then('./config/middleware.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
 
 app.listen(3000, ()=>{
     console.log('back end show');
