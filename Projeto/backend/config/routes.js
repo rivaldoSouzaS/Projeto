601773100014cf833ba002c5ba9260c6e59ee9ae
rivaldo souza <rivaldosilva@si.fiponline.edu.br>
2022-02-06 16:59:18 -0300

    validation save edit get

--- Projeto/backend/config/routes.js
+++ Projeto/backend/config/routes.js
@@ -2,4 +2,8 @@ module.exports = app =>{
 
     app.route('/users')
         .post(app.api.user.save)
+        .get(app.api.user.get)
+
+    app.route('/users/:id')
+        .put(app.api.user.save)
 }
